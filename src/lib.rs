5b54224cbadf7a99b6fd881ddc42cf4051b5996c
Humac <hoomaac@gmail.com>
2025-04-01 21:33:31 +0200

    Lexing whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 use core::fmt;
 use std::borrow::Cow;
 
-use miette::{miette, LabeledSpan, Error};
+use miette::{Error, LabeledSpan, miette};
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Token<'a> {
+pub struct Token<'a> {
+    token_type: TokenType,
+    original: &'a str,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenType {
     LeftParen,
     RightParen,
     LeftBrace,
@@ -24,9 +30,9 @@ pub enum Token<'a> {
     Bang,
     Slash,
     Equal,
-    String(&'a str),
-    Number(&'a str, f64),
-    Identifier(&'a str),
+    String,
+    Number(f64),
+    Identifier,
     And,
     Class,
     Else,
@@ -46,45 +52,46 @@ pub enum Token<'a> {
 
 impl<'a> fmt::Display for Token<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                Token::LeftParen=> write!(f,"LEFT_PAREN ) null"),
-                Token::RightParen=> write!(f, "RIGHT_PAREN ( null"),
-                Token::LeftBrace=> write!(f, "LEFT_BRACE {{ null"),
-                Token::RightBrace=> write!(f, "RIGHT_BRACE }} null"),
-                Token::Comma=> write!(f, "COMMA , null"),
-                Token::Dot=> write!(f, "DOT . null"),
-                Token::Minus=> write!(f, "MINUS - null"),
-                Token::Plus=> write!(f, "PLUS + null"),
-                Token::Semicolon=> write!(f, "SEMICOLON ; null"),
-                Token::Star=> write!(f, "STAR * null"),
-                Token::String(s)=> write!(f,"STRING \"{s}\" {}",Token::unescpaed(s)),
-                Token::Number(lit, i) => write!(f, "NUMBER {lit} {i}"),
-                Token::Identifier(n) => write!(f, "IDENTIFIER {n} null"),
-                Token::And => write!(f, "And And null"),
-                Token::Class => write!(f, "CLASS class null"),
-                Token::Else => write!(f, "ELSE else null"),
-                Token::False => write!(f, "FALSE false null"),
-                Token::For => write!(f, "FOR for null"),
-                Token::Fun => write!(f, "FUN fun null"),
-                Token::If => write!(f, "IF if null"),
-                Token::Nil => write!(f, "NIL nil null"),
-                Token::Or => write!(f, "OR or null"),
-                Token::Return => write!(f, "RETURN return null"),
-                Token::Super => write!(f, "SUPER super null"),
-                Token::This => write!(f, "THIS this null"),
-                Token::True => write!(f, "TRUE true null"),
-                Token::Var => write!(f, "VAR var null"),
-                Token::While => write!(f, "WHILE while null"),
-                Token::BandEqual => write!(f, "BANDEQUAL != null"),
-                Token::EqualEqual => write!(f, "EQUALEQUAL == null"),
-                Token::LessEqual => write!(f, "LESSEQUAL <= null"),
-                Token::GreaterEqual => write!(f, "GREATEREQUAL >= null"),
-                Token::Less => write!(f, "Less < null"),
-                Token::Greater => write!(f, "Greater > null"),
-                Token::Slash => write!(f, "SLASH / null"),
-                Token::Bang => write!(f, "BANG ! null"),
-                Token::Equal => write!(f, "EQUAL = null"),
-            }
+        let original = self.original;
+        match self.token_type {
+            TokenType::LeftParen => write!(f, "LEFT_PAREN {original} null"),
+            TokenType::RightParen => write!(f, "RIGHT_PAREN {original} null"),
+            TokenType::LeftBrace => write!(f, "LEFT_BRACE {original} null"),
+            TokenType::RightBrace => write!(f, "RIGHT_BRACE {original} null"),
+            TokenType::Comma => write!(f, "COMMA {original} null"),
+            TokenType::Dot => write!(f, "DOT {original} null"),
+            TokenType::Minus => write!(f, "MINUS {original} null"),
+            TokenType::Plus => write!(f, "PLUS {original} null"),
+            TokenType::Semicolon => write!(f, "SEMICOLON {original} null"),
+            TokenType::Star => write!(f, "STAR {original} null"),
+            TokenType::String => write!(f, "STRING {original} {}", Token::unescpaed(original)),
+            TokenType::Number(n) => write!(f, "NUMBER {original} {n}"),
+            TokenType::Identifier => write!(f, "IDENTIFIER {original} null"),
+            TokenType::And => write!(f, "And {original} null"),
+            TokenType::Class => write!(f, "CLASS {original} null"),
+            TokenType::Else => write!(f, "ELSE {original} null"),
+            TokenType::False => write!(f, "FALSE {original} null"),
+            TokenType::For => write!(f, "FOR for {original}"),
+            TokenType::Fun => write!(f, "FUN {original} null"),
+            TokenType::If => write!(f, "IF {original} null"),
+            TokenType::Nil => write!(f, "NIL {original} null"),
+            TokenType::Or => write!(f, "OR {original} null"),
+            TokenType::Return => write!(f, "RETURN {original} null"),
+            TokenType::Super => write!(f, "SUPER {original} null"),
+            TokenType::This => write!(f, "THIS {original} null"),
+            TokenType::True => write!(f, "TRUE {original} null"),
+            TokenType::Var => write!(f, "VAR {original} null"),
+            TokenType::While => write!(f, "WHILE {original} null"),
+            TokenType::BandEqual => write!(f, "BANDEQUAL {original} null"),
+            TokenType::EqualEqual => write!(f, "EQUALEQUAL {original} null"),
+            TokenType::LessEqual => write!(f, "LESSEQUAL {original} null"),
+            TokenType::GreaterEqual => write!(f, "GREATEREQUAL {original} null"),
+            TokenType::Less => write!(f, "Less {original} null"),
+            TokenType::Greater => write!(f, "Greater {original} null"),
+            TokenType::Slash => write!(f, "SLASH {original} null"),
+            TokenType::Bang => write!(f, "BANG {original} null"),
+            TokenType::Equal => write!(f, "EQUAL {original} null"),
+        }
     }
 }
 
@@ -105,7 +112,7 @@ impl<'a> Lexer<'a> {
         Self {
             whole: input,
             rest: input,
-            byte:0,
+            byte: 0,
         }
     }
 }
@@ -114,60 +121,85 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Result<Token<'a>, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut chars = self.rest.chars();        
-        let c = chars.next()?;
-        self.byte += c.len_utf8();
-        self.rest = chars.as_str();
+        loop {
+            let mut chars = self.rest.chars();
+            let c = chars.next()?;
+            let c_str = &self.rest[..c.len_utf8()];
+            let c_onwards = self.rest;
+            self.byte += c.len_utf8();
+            self.rest = chars.as_str();
 
-        enum Started<'a> {
-            String,
-            Number,
-            Identifier,
-            IfEqualElse(Token<'a>, Token<'a>),
-        }
+            let get_token = |token_type: TokenType| {
+                Some(Ok(Token {
+                    token_type,
+                    original: c_str,
+                }))
+            };
 
-        let started = match c {
-            '('=> return Some(Ok(Token::LeftParen)),
-            ')'=> return Some(Ok(Token::RightParen)),
-            '{'=> return Some(Ok(Token::LeftBrace)),
-            '}'=> return Some(Ok(Token::RightBrace)),
-            ','=> return Some(Ok(Token::Comma)),
-            '.'=> return Some(Ok(Token::Dot)),
-            '-'=> return Some(Ok(Token::Minus)),
-            '+'=> return Some(Ok(Token::Plus)),
-            ';'=> return Some(Ok(Token::Semicolon)),
-            '*'=> return Some(Ok(Token::Star)), 
-            '"'=> Started::String,
-            '<' => Started::IfEqualElse(Token::LessEqual, Token::Equal),
-            '>' => Started::IfEqualElse(Token::GreaterEqual, Token::Equal),
-            '=' => Started::IfEqualElse(Token::EqualEqual, Token::Equal), 
-            '!' => Started::IfEqualElse(Token::BandEqual, Token::Bang),
-            '0'..='9' => Started::Number,
-            'a'..='z' | '_' => Started::Identifier,
-            _ => return Some(Err(miette::miette!(
-                labels = vec![
-                    LabeledSpan::at(self.byte - c.len_utf8()..self.byte + c.len_utf8(), "this character"),
-                ],
-                "Unexpected token '{c}' in input",
-            ).with_source_code(self.whole.to_string())))
-        };
-
-        match started {
-            Started::String => todo!(),
-            Started::Number => todo!(),
-            Started::Identifier => todo!(),
-            Started::IfEqualElse(yes, no) => {
-                if self.rest.starts_with("=") {
-                    self.rest = &self.rest[1..];
-                    self.byte += 1;
-                    return Some(Ok(yes));
-                }
-                else {
-                    return Some(Ok(no));
-                }
+            enum Started {
+                String,
+                Number,
+                Identifier,
+                IfEqualElse(TokenType, TokenType),
             }
-        }
-        
-    }
 
+            let started = match c {
+                '(' => return get_token(TokenType::LeftParen),
+                ')' => return get_token(TokenType::RightParen),
+                '{' => return get_token(TokenType::LeftBrace),
+                '}' => return get_token(TokenType::RightBrace),
+                ',' => return get_token(TokenType::Comma),
+                '.' => return get_token(TokenType::Dot),
+                '-' => return get_token(TokenType::Minus),
+                '+' => return get_token(TokenType::Plus),
+                ';' => return get_token(TokenType::Semicolon),
+                '*' => return get_token(TokenType::Star),
+                '/' => return get_token(TokenType::Slash),
+                '"' => Started::String,
+                '<' => Started::IfEqualElse(TokenType::LessEqual, TokenType::Equal),
+                '>' => Started::IfEqualElse(TokenType::GreaterEqual, TokenType::Equal),
+                '=' => Started::IfEqualElse(TokenType::EqualEqual, TokenType::Equal),
+                '!' => Started::IfEqualElse(TokenType::BandEqual, TokenType::Bang),
+                '0'..='9' => Started::Number,
+                'a'..='z' | '_' => Started::Identifier,
+                c if c.is_whitespace() => continue,
+                _ => {
+                    return Some(Err(miette::miette!(
+                        labels = vec![LabeledSpan::at(
+                            self.byte - c.len_utf8()..self.byte + c.len_utf8(),
+                            "this character"
+                        ),],
+                        "Unexpected TokenType '{c}' in input",
+                    )
+                    .with_source_code(self.whole.to_string())));
+                }
+            };
+
+            break match started {
+                Started::String => todo!(),
+                Started::Number => todo!(),
+                Started::Identifier => todo!(),
+                Started::IfEqualElse(yes, no) => {
+                    self.rest = self.rest.trim_start();
+                    let trimmed = c_onwards.len() - self.rest.len() - 1;
+                    self.byte += trimmed;
+
+                    if self.rest.starts_with("=") {
+                        let span = &c_onwards[..c.len_utf8() + trimmed + 1];
+                        self.rest = &self.rest[1..];
+                        self.byte += 1;
+                        Some(Ok(Token {
+                            token_type: yes,
+                            original: span,
+                        }))
+                    } else {
+                        Some(Ok(Token {
+                            token_type: no,
+                            original: c_str,
+                        }))
+                    }
+                }
+            };
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{fs, io, path::PathBuf};
 use clap::{Parser, Subcommand};
 use loxer::Lexer;
 use miette::{Context, IntoDiagnostic};
+use std::{fs, io, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -12,29 +12,27 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Tokenize {
-        filename: PathBuf
-    },
+    Tokenize { filename: PathBuf },
 }
 
 fn main() -> miette::Result<()> {
     let args = Args::parse();
-    parse(&args) 
+    parse(&args)
 }
 
 fn parse(args: &Args) -> miette::Result<()> {
     match &args.command {
-        Commands::Tokenize{filename} => {
+        Commands::Tokenize { filename } => {
             let file_contents = fs::read_to_string(&filename)
                 .into_diagnostic()
                 .wrap_err_with(|| format!("failed to read file: {}", filename.display()))?;
-            
+
             for token in Lexer::new(&file_contents) {
                 let token = token?;
                 println!("{token}");
             }
             println!("EOF null");
-        },
+        }
     }
 
     Ok(())
